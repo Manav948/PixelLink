4412c740c9ea1460a5ae370b9f67b32723c23c08
Manav948 <valanimanav61@gmail.com>
2026-07-25 23:52:53 +0530

    setup Prisma 7 adapter and fix request body parsing and implement longurl route

---
+++ app/api/url/route.ts
@@ -0,0 +1,40 @@
+import { shortLinkService } from "@/services/shortLink.service";
+import { urlValidator } from "@/validators/url.validator";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+    try {
+        const body = await req.json();
+        const result = urlValidator.safeParse(body);
+
+        if (!result.success) {
+            return NextResponse.json({
+                success: false,
+                message: "Invalid payload",
+                errors: result.error.format()
+            },
+            {
+                status: 400
+            });
+        }
+
+        const shortLinkUrl = await shortLinkService.generateUniqueSlug(
+            result.data.url
+        );
+        return NextResponse.json({
+            success : true,
+            data : shortLinkUrl
+        },{
+            status : 201
+        })
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({
+            success: false,
+            message: "Internal Server Error",
+        },
+            {
+                status: 500
+        })
+    }
+}
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,16 @@
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
-const globalForPrisma = globalThis as unknown as {prisma: PrismaClient | undefined};
+const connectionString = process.env.DATABASE_URL;
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 
-if(process.env.NODE_ENV !== "production") {
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined };
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") {
     globalForPrisma.prisma = prisma;
 }
